c67a8459c5a89a08a3e06c88168ca4eb26fbb00e
Abdul Al kadri <abed.al.kadri@hotmail.com>
2024-01-22 22:47:53 +0100

    Update DoraGame.py

--- Runner/DoraGame.py
+++ Runner/DoraGame.py
@@ -8,16 +8,16 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 35)
 
-script_dir = os.path.dirname(__file__)
 
-skyImage_path = os.path.join(script_dir, 'Graphics', 'Sky.png')
-groundImage_path = os.path.join(script_dir, 'Graphics', 'ground.png')
-snail_path = os.path.join(script_dir, 'Graphics', 'Snail', 'snail1.png')
-
-ground_surface = pygame.image.load(groundImage_path)
-sky_surface = pygame.image.load(skyImage_path)
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
 text_surface = test_font.render('test', True, 'purple')
-snail_surface = pygame.image.load(snail_path)
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png')
+player_rect = player_surf.get_rect(midbottom = (80, 300))
+
 
 while True:
     for event in pygame.event.get():
@@ -28,7 +28,12 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (210, 0))
-    screen.blit(snail_surface, (600, 250))
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800 
+    screen.blit(snail_surf, snail_rect)
+    player_rect.left += 2
+    screen.blit(player_surf,player_rect)
 
     pygame.display.update()
     clock.tick(60)
